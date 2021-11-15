25bf609c4a23da1db10a0adec799fd4655ba5e3c
ronaldchatelier <62119544+ronaldchatelier@users.noreply.github.com>
2021-11-15 15:31:47 -0500

    Update to Unity Project

--- Video Game Project (COP 3003)/Assets/Movement.cs
+++ Video Game Project (COP 3003)/Assets/Movement.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Jumping : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public float MovementSpeed = 1;
+    public float movementSpeed = 1;
+    
+    
     private void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     private void Update()
     {
-
-        // Left and Right arrow Keys
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * movementSpeed;
     }
-}
+
+        void Jump()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            }
+
+
+        }
+    }
+
